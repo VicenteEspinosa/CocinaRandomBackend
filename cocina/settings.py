752fec946e990744d4d8914cf98c790d3d44dd1a
Vicente Espinosa <vnespinosa@uc.cl>
2021-08-07 12:35:38 -0400

    feat(settings): add cors

--- cocina/settings.py
+++ cocina/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cocinaapp'
+    'cocinaapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+# CORS_ALLOWED_ORIGINS = [
+#     'https://panel.osam.cl',
+#     'https://dev.panel.osam.cl'
+# ] 
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'cocina.urls'
 
