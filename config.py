c804c7e76e4cfa8e9395c02e44431f297ebce679
Vicente Espinosa <53794581+vnespinosa@users.noreply.github.com>
2021-08-21 00:33:53 -0400

    feat(create_recipe): allow to upload pictures (#21)

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ os.environ.setdefault('B_DJANGO_SECRET_KEY', 'development-key')
 DJANGO_SECRET_KEY = os.environ['B_DJANGO_SECRET_KEY']
 MONGO_URI = os.environ['B_MONGO_URI']
 DEBUG_SET = os.environ['DEBUG']
+CLOUD_NAME = os.environ['CLOUD_NAME']
+API_KEY = os.environ['API_KEY']
+API_SECRET = os.environ['API_SECRET']
