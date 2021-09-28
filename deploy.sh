b53794b1169bc05992b3a17109f072252c7f50a4
Vicente Espinosa <53794581+vnespinosa@users.noreply.github.com>
2021-09-28 10:51:24 -0300

    refactor(repo): delete unnecesary file (#25)

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-sudo git pull origin main
-source .env
-poetry install
-poetry run python cocina/manage.py makemigrations
-poetry run python cocina/manage.py migrate
-poetry run python cocina/manage.py collectstatic
